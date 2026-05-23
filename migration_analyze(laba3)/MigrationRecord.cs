4e7f5b48712bfb583fead9daf456cc6973d1ddee
shino812 <ivanshirokov228@gmail.com>
2026-05-23 11:49:19 +0300

    Добавлена загрузка и анализ CSV данных

---
+++ migration_analyze(laba3)/MigrationRecord.cs
@@ -0,0 +1,11 @@
+﻿namespace migration_analyze_laba3_
+{
+    public class MigrationRecord
+    {
+        public int Year { get; set; }
+
+        public int Immigrants { get; set; }
+
+        public int Emigrants { get; set; }
+    }
+}
